6d21700a47e40127e06f192e47dd2db3154208ec
Chandra Kumar <chandrakumar.c@nium.com>
2026-04-26 06:47:27 +0530

    Week 01 Day 01 Assignments

---
+++ tests/Week 01/Day 01/primitiveDataTypesWithLet.js
@@ -0,0 +1,13 @@
+// Declare variables using let
+let firstName = "Aadhi";
+let companyName = "TechCorp";
+let mobileNumber = 8392893289;
+let isAutomation = false;
+let hasPlaywright; // not assigned
+
+// Print values and their types
+console.log("firstName:", firstName, "| type:", typeof firstName);
+console.log("companyName:", companyName, "| type:", typeof companyName);
+console.log("mobileNumber:", mobileNumber, "| type:", typeof mobileNumber);
+console.log("isAutomation:", isAutomation, "| type:", typeof isAutomation);
+console.log("hasPlaywright:", hasPlaywright, "| type:", typeof hasPlaywright);
---
+++ tests/Week 01/Day 01/primitiveDataTypesWithVar.js
@@ -0,0 +1,13 @@
+// Declare variables using var
+var firstName = "Aadhi";
+var companyName = "TechCorp";
+var mobileNumber = 8392893289;
+var isAutomation = false;
+var hasPlaywright; // not assigned
+
+// Print values and their types
+console.log("firstName:", firstName, "| type:", typeof firstName);
+console.log("companyName:", companyName, "| type:", typeof companyName);
+console.log("mobileNumber:", mobileNumber, "| type:", typeof mobileNumber);
+console.log("isAutomation:", isAutomation, "| type:", typeof isAutomation);
+console.log("hasPlaywright:", hasPlaywright, "| type:", typeof hasPlaywright);
